f389adf15ff9a0a4492bc087cedceffdd703e0f1
Josipa <56545566+jjosipa404@users.noreply.github.com>
2020-05-28 12:35:25 +0200

    Update IdlePyOpenGL.bat

--- IdlePyOpenGL.bat
+++ IdlePyOpenGL.bat
@@ -1,1 +1,1 @@
-start "" C:\Users\Korisnik\Anaconda3\envs\pygame_opengl_env\python.exe -c "from idlelib.pyshell import main; main()"
+start "" C:\Users\User\Anaconda3\envs\pygame_opengl_env\python.exe -c "from idlelib.pyshell import main; main()"
